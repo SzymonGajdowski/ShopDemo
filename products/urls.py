83f11eee74b39e7075714face12c6d032fdccb92
SzymonGajdowski <sz.gajdowski@gmail.com>
2020-09-10 22:42:03 +0200

    Add CRUD

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.home_view, name='product_home'),
     path('about/', views.about_view, name='product_about'),
     path('contact/', views.contact_view, name='product_contact'),
+    path('create_product/', views.create_product, name='product_create'),
+    path('edit_product/<int:pk>/', views.edit_product, name='product_edit'),
+    path('delete_product/<int:pk>/', views.delete_product, name='product_delete'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Category, Product
+from .forms import ProductForm
 
 def home_view(request):
     products = Product.objects.all()
@@ -15,3 +16,37 @@ def about_view(request):
 
 def contact_view(request):
     return render(request, 'products/contact.html')
+
+def create_product(request):
+
+    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'products/create_form.html', context)
+
+def edit_product(request, pk):
+
+    product = Product.objects.get(id=pk)
+    form = ProductForm(instance=product)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'products/create_form.html', context)
+
+def delete_product(request, pk):
+
+    product = Product.objects.get(id=pk)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('/')
+
+    return render(request, 'products/delete_form.html')
